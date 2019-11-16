1970bd3af58579698d91a513e7857bded20b60ee
Laia <laia.d.c@hotmail.com>
2019-11-16 12:19:13 +0100

    Fixed error with Lasso

--- lasso.py
+++ lasso.py
@@ -9,20 +9,22 @@ LASSO REGRESSION
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
 import warnings
 warnings.simplefilter('ignore')
 
 from sklearn.linear_model import Ridge, RidgeCV, ElasticNet, LassoCV, LassoLarsCV
 from sklearn.model_selection import cross_val_score
 
-def rmse_cv(model):
-    rmse = np.sqrt(cross_val_score(model, X_train, y_train, scoring="mean_squared_error", cv = 5))
+def rmse_cv(model, X_train, y_train):
+    rmse = np.sqrt(cross_val_score(model, X_train, y_train, scoring='accuracy', cv = 5))
     return(rmse)
 
 
 def Lasso_importance(X_train, y_train):
-    model_lasso = LassoCV(alphas = [1, 0.1, 0.001, 0.0005]).fit(X_train, y_train)
-    print('RMSE :',rmse_cv(model_lasso).mean())
+    model_lasso = LassoCV(alphas = [1, 0.1, 0.001, 0.0005],random_state=42).fit(X_train, y_train)
+   # print('RMSE :',rmse_cv(model_lasso, X_train, y_train).mean())
     coef = pd.Series(model_lasso.coef_, index = X_train.columns)
     print("Lasso picked " + str(sum(coef != 0)) + " variables and eliminated the other " +  str(sum(coef == 0)) + " variables")
     print(coef.head())
@@ -33,3 +35,4 @@ def Lasso_importance(X_train, y_train):
     matplotlib.rcParams['figure.figsize'] = (8.0, 10.0)
     imp_coef.plot(kind = "barh")
     plt.title("Coefficients in the Lasso Model")
+    return coef
