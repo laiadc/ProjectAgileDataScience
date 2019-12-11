b09dbd3b4c4f8942fc94bc627ec57460934ef2cb
aliarachan <kaala_llunaa@hotmail.com>
2019-12-11 20:40:56 +0000

    Added SSL and Cors

--- backend/server/runServer.sh
+++ backend/server/runServer.sh
@@ -4,5 +4,5 @@ rm SKM/ -r
 cp ../../Model/SKMlib/SKM . -r
 source  .flaskEnv/bin/activate
 export FLASK_APP=server.py
-python3.6 -m flask run --host=0.0.0.0 
+python3.6 -m flask run --host=0.0.0.0 --cert=adhoc
 source deactivate
